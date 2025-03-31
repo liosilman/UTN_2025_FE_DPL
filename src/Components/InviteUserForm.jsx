2c7d8594228478306fa250dcb060f1493adfbcb4
lionDev <liosilman02@gmail.com>
2025-03-31 18:48:31 -0300

    Vercel fix

--- src/Components/InviteUserForm.jsx
+++ src/Components/InviteUserForm.jsx
@@ -1,4 +1,3 @@
-"use client"
 
 import { useState } from "react"
 import { get, post } from "../utils/fetching/fetching.utils"
--- src/hooks/useApiRequest.jsx
+++ src/hooks/useApiRequest.jsx
@@ -1,4 +1,4 @@
-"use client"
+
 
 import { useState, useCallback } from "react"
 import { formatError } from "../utils/error.utils"
